7b851a704ae4acc14741b4d419bba8168646bcdf
voltcap <121193901+voltcap@users.noreply.github.com>
2025-05-15 14:02:00 +0300

    Create Gdrive_mount.py

---
+++ Gdrive_mount.py
@@ -0,0 +1,6 @@
+drive.mount('/content/drive')
+
+CheckpointBruv = "/content/drive/MyDrive/DATA/Checkpoints"
+Visualise_innit = "/content/drive/MyDrive/DATA/visualisations"
+os.makedirs(CheckpointBruv, exist_ok=True)
+os.makedirs(Visualise_innit, exist_ok=True)
